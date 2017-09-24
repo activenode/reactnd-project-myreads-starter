790b56eef427df829d977ea38f3daf39de057feb
David Lorenz <info@activenode.de>
2017-09-24 18:57:41 +0200

    feat: added loading indicator component

---
+++ src/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Loader() {
+  return (<div className="la-ball-newton-cradle">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>);
+}
+
+export default Loader;
