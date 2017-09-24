0e0f90fb8c18f1f363ff5f3fd3c5c3936594847e
David Lorenz <info@activenode.de>
2017-09-24 09:42:31 +0200

    Added fading mechanism

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './App.css';
+import './Anim.css';
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
 import * as BooksAPI from './BooksAPI';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 class BooksApp extends React.Component {
   state = {
@@ -13,33 +15,53 @@ class BooksApp extends React.Component {
   componentDidMount() {
     BooksAPI.getAll().then(books => {
       this.setState({books: books});
-    }).catch(() => this.setState({books: []}));
+      console.log(books);
+    }).catch(() => this.setState({books: []})); //TODO: add errorneous on catch()
   }
 
   updateBook = function(book) {
-    BooksAPI.update(book, book.shelf).then(() => {
-      this.setState(state => {
-        return {
-          books: state.books.map(bookItem => {
-            if (bookItem.id === book.id) {
-              return book;
-            }
-            return bookItem;
-          })
-        };
-      });
+    let prevState;
+
+    //now set OPTIMISTIC state first
+    this.setState(state => {
+      prevState = state;
+
+      return {
+        books: state.books.map(bookItem => {
+          if (bookItem.id === book.id) {
+            return book;
+          }
+          return bookItem;
+        })
+      };
+    });
+
+    BooksAPI.update(book, book.shelf).catch(()=>{
+      this.setState(prevState); //reset optimistic state
     });
   }
 
   render() {
     return (
       <BrowserRouter>
-        <div className="app">
-          <Route exact path="/" render={() => (
+        <Route render={props=>{
+          let isSearch = false;
+          if (props.location.pathname.match('/search')) {
+            isSearch = true;
+          }
+
+          return (
+          <div className={`app ${isSearch ? 'is-search' : ''}`}>
             <ListBooks books={this.state.books} updateBook={book => this.updateBook(book)} />
-          )} />
-          <Route exact path="/search" component={SearchBooks} />
-        </div>
+
+            <CSSTransitionGroup
+              transitionName="fade"
+              transitionEnterTimeout={150}
+              transitionLeaveTimeout={150}>
+              <Route exact path="/search" key={props.location.key} location={props.location} component={SearchBooks} />
+            </CSSTransitionGroup>
+          </div>)
+        }} />
       </BrowserRouter>
     );
   }
